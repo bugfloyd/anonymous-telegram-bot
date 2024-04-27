8fa94259d098fb5b11f568b752895c84532bfdda
Yashar Hosseinpour <yashar.hv@gmail.com>
2024-04-27 16:44:48 +0200

    Add CI base infra
    GitHub openid connect provider
    IAM role for GitHub actions

--- infra/init/variables.tf
+++ infra/init/variables.tf
@@ -16,3 +16,8 @@ variable "init_lambda_bucket" {
   description = "The AWS S3 bucket for storing lambda function codes"
   type        = string
 }
+
+variable "github_repo" {
+  description = "Github repo to be used to allow github actions to have access to the AWS account via OIDC in this format: <GITHUB_OWNER>/<GITHUB_REPO>"
+  type        = string
+}
